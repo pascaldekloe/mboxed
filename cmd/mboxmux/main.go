04f5e912110f0d3f2795bdcc8125a0a9647f3fe6
Pascal S. de Kloe <pascal@quies.net>
2022-07-03 15:49:24 +0200

    token omission

--- cmd/mboxmux/main.go
+++ cmd/mboxmux/main.go
@@ -54,13 +54,19 @@ var name = os.Args[0]
 
 // Command Invocation Options
 var (
-	headerFlag = flag.String("header", "", "Defines the header (`name`) used for file distribution.")
-	outDirFlag = flag.String("d", ".", "Sets the `directory` for output files.")
+	headerFlag = flag.String("header", "", "Define the header (`name`) used for file distribution.")
+	outDirFlag = flag.String("d", ".", "Set the `directory` for output files.")
 	escapeFlag = flag.String("escape", "_", fmt.Sprintf("Sets the `replacement` for %q occurences in output files.", filepath.Separator))
 )
 
+var tokenTrims []string
+
 func main() {
 	log.SetFlags(0)
+	flag.Func("tokentrim", "Add a `pattern` for token omission on the output files. The first character in the pattern defines the token separator, and the remainder sets the token to be excluded. E.g., -tokentrim ,Opened omits any Opened occurences in a comma-separated list, i.e., Inbox,Opened,Important would become Inbox,Important. Multiple tokentrim arguments are applied in conjuntion.", func(s string) error {
+		tokenTrims = append(tokenTrims, s)
+		return nil
+	})
 	flag.Parse()
 
 	err := os.MkdirAll(*outDirFlag, 0o755)
@@ -80,7 +86,11 @@ func main() {
 	switch {
 	case *headerFlag != "":
 		split.KeyFunc = func(msg *mail.Message, raw []byte) string {
-			return msg.Header.Get(*headerFlag)
+			s := msg.Header.Get(*headerFlag)
+			for _, pattern := range tokenTrims {
+				s = trimToken(s, pattern)
+			}
+			return s
 		}
 	default:
 		log.Fatalf("%s: no split key defined: use the header flag", name)
@@ -103,4 +113,26 @@ func main() {
 	}
 
 	os.Exit(failN)
+}
+
+func trimToken(s, exclude string) string {
+	if exclude == "" {
+		return s
+	}
+	separator := exclude[:1]
+	token := exclude[1:]
+
+	// fast-path for token absense
+	if strings.Index(s, token) < 0 {
+		return s
+	}
+
+	tokens := strings.Split(s, separator)
+	for i := len(tokens) - 1; i > 0; i-- {
+		if strings.TrimSpace(tokens[i]) == token {
+			// delete
+			tokens = append(tokens[:i], tokens[i+1:]...)
+		}
+	}
+	return strings.Join(tokens, separator)
 }
